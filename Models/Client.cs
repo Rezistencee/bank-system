323f80ed37deb6d5562d0227202842ff9becfb28
Rezistencee <86205500+Rezistencee@users.noreply.github.com>
2024-05-15 16:22:57 +0300

    Complete MainWindow.axaml layout.

--- Models/Client.cs
+++ Models/Client.cs
@@ -7,18 +7,20 @@ namespace BankSystem.Models
         public static int ID { get; private set; }
         public string Login { get; set; }
         public string Password { get; set; }
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public string ERDPOU { get; set; }
+        public string Name { get; set; }
+        public string TelephoneNumber { get; set; }
+        public string EDRPOU { get; set; }
+        public string Address { get; set; }
 
         public Client()
         {
             ID = ID++;
             Login = String.Empty;
             Password = String.Empty;
-            Firstname = String.Empty;
-            Lastname = String.Empty;
-            ERDPOU = String.Empty;
+            Name = String.Empty;
+            EDRPOU = String.Empty;
+            TelephoneNumber = String.Empty;
+            Address = String.Empty;
         }
     }
 }
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Reactive;
 using System.Windows.Input;
 using Avalonia.Controls;
@@ -10,20 +12,54 @@ namespace BankSystem.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
+    private ObservableCollection<Client> _clients;
     private ISession _currentSession;
     
     public Client CurrentClient
     {
         get => _currentSession.Client;
     }
+
+    public ObservableCollection<Client> Clients
+    {
+        get => _clients;
+        set
+        {
+            _clients = value;
+        }
+    }
     
     public ICommand OpenAdminWindowCommand { get; }
     
     public MainWindowViewModel()
     {
+        _clients = new ObservableCollection<Client>();
+        
         OpenAdminWindowCommand = ReactiveCommand.Create(OpenAdminWindow);
 
         _currentSession = SingletonSession.GetInstance().CurrentSession;
+        
+        _clients.Add(new Client
+        {
+            Name = "Oleksii Khodakovskiy",
+            EDRPOU = "362362"
+        });
+        
+        _clients.Add(new Client
+        {
+            Name = "Andriy Danylov",
+            EDRPOU = "362362"
+        });
+        _clients.Add(new Client
+        {
+            Name = "Ivan",
+            EDRPOU = "362362"
+        });
+        _clients.Add(new Client
+        {
+            Name = "Zolik",
+            EDRPOU = "362362"
+        });
     }
 
     private void OpenAdminWindow()
